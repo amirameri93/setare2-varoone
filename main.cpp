e0580d9b61dbcb360f9b527588fcddbddf736dc0
amirameri93 <100429387+amirameri93@users.noreply.github.com>
2022-05-24 12:45:17 +0430

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+ {
+ 	for(int i=1;i<=4;i++)
+ 	{
+ 		for(int k=1;k<=i;k++)
+ 	{cout<<" ";
+		 }	
+ 		for(int j=(i-1)*2;j<=6;j++)
+ 		{
+ 				 cout<<"x";
+ 			
+		 }
+		 
+	cout<<endl;
+	 }
+	 
+ 	
+	return 0;
+}
